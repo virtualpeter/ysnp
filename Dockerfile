3ea627e9290d6c9613bc1ed774c1450f295a234f
Peter Viertel <peter.viertel@nab.com.au>
2018-12-23 01:25:49 +1100

    seems to be working but no tests yet

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,25 @@
 FROM scratch
-ARG LISTEN=:8080
-ENV LISTEN=${LISTEN}
+
+#with these defaults it will redirect to the host and path from the request, just strips port and query flags
+#and sets protocol to https
+
+ENV PORT=8080
+ENV STATUS=301
+ENV TARGET_HOST=""
+ENV TARGET_PORT=""
+ENV TARGET_PROTO="https"
+ENV TARGET_PATH=""
+ENV PASSQUERY="false"
+
 ADD ysnp /
-EXPOSE ${LISTEN}
-CMD ["/ysnp","-listen","${LISTEN}"]
+
+EXPOSE $PORT
+
+CMD [ "/ysnp","-listen",":$PORT",\
+      "-target_proto","$TARGET_PROTO",\
+      "-target_host","$TARGET_HOST",\
+      "-target_port","$TARGET_PORT",\
+      "-target_path","$TARGET_PATH",\
+      "-passquery","$PASSQUERY",\
+      "-status","$STATUS"\
+    ]
--- ysnp.go
+++ ysnp.go
@@ -5,31 +5,66 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"strings"
 
 	httplogger "github.com/gleicon/go-httplogger"
 )
 
 func redirector(w http.ResponseWriter, r *http.Request) {
-	// log.Printf("req: %+v \n", r)
+	log.Printf("req: %+v \n", r)
 	u := r.URL
-	host, _, err := net.SplitHostPort(r.Host)
-	if err == nil {
-		u.Host = host
+	if *targetHost == "" {
+		host, _, err := net.SplitHostPort(r.Host)
+		if err == nil {
+			u.Host = host
+		} else {
+			u.Host = r.Host
+		}
 	} else {
-		u.Host = r.Host
+		u.Host = *targetHost
 	}
-	u.Scheme = "https"
+	if *targetPort != "" {
+		host := strings.Split(u.Host, ":")[0]
+		u.Host = host + ":" + *targetPort
+	}
+	if *targetProto == "http" {
+		u.Scheme = "http"
+	} else {
+		u.Scheme = "https"
+	}
+	if *targetPath != "" {
+		u.Path = *targetPath
+	}
+	if !*passQuery {
+		u.RawQuery = ""
+	}
+
 	// log.Printf("location: %s\n", u.String())
-	http.Redirect(w, r, u.String(), http.StatusMovedPermanently)
+
+	log.Printf("redirect: %+v \n", u)
+
+	http.Redirect(w, r, u.String(), *redirectStatus)
 }
+
+var httpAddr = flag.String("listen", ":8080", "TCP host:port to listen on for http requests")
+var targetProto = flag.String("target_proto", "https", "protocol to redirect to, so far the only other supported option is http")
+var targetHost = flag.String("target_host", "", "hardcode this domainname in redirect instead of passing on request")
+var targetPort = flag.String("target_port", "", "port to use in redirect, default is to not have an explicit port")
+var targetPath = flag.String("target_path", "", "hardcode this path in redirect, default means use request path")
+var passQuery = flag.Bool("passquery", false, "set true if you want to pass request query parameters in redirect")
+var redirectStatus = flag.Int("status", http.StatusMovedPermanently, "http status 3xx code to return")
 
 func main() {
 
-	httpAddr := flag.String("listen", ":8080", "TCP host:port to listen on for http requests")
 	flag.Parse()
 
-	// dropUri := flag.Bool("dropuri", false, "set true if you want to discard requested uri in redirect")
+	if !isValidRedirectStatus(*redirectStatus) {
+		log.Fatal("redirect status must be one of 301,302,307,308")
+	}
+
 	log.Printf("ysnp: listening on host %s\n", *httpAddr)
+	log.Printf("ysnp: target pattern %s://%s:%s/%s\n", *targetProto, *targetHost, *targetPort, *targetPath)
+
 	serveMux := http.NewServeMux()
 
 	serveMux.HandleFunc("/", redirector)
@@ -40,4 +75,14 @@ func main() {
 	}
 	log.Fatal(httpSrv.ListenAndServe())
 
+}
+
+func isValidRedirectStatus(f int) bool {
+	answer := false
+	for _, x := range []int{301, 302, 307, 308} {
+		if x == f {
+			answer = true
+		}
+	}
+	return answer
 }
